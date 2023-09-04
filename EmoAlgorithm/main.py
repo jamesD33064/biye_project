b50c14a40facac7cfbc357e9b2214695760ef6d5
jamesD33064 <james0120160379@gmail.com>
2023-09-04 16:36:03 +0800

    情緒對應演算法V2

--- EmoAlgorithm/main.py
+++ EmoAlgorithm/main.py
@@ -1,45 +1,40 @@
-
+from random import random
 def get_current_emotion(emo):
-    # 情緒判斷邏輯
-    emotional_list = [
+    postiveFlag = None # 1正向 0中性 -1負向
+
+    postiveLinear = [
         434, # 平靜一 nutral
         700, # 期待 happy
-        2849, # 起伏較大 激動 angry
         900, # 期待 愉悅 surprise
-        1100, # 快哭了 sad
-        2600, # 害怕 fear
+        2849, # 一點小激動
     ]
-    
-    if max(emo) == 0.0:     # 全部0
-        current_emotional_number = emotional_list[emotional_list.index(434)]
-    elif int(max(emo)) == 1:    # 有1的
-        current_emotional_number = emotional_list[emo.index(max(emo))]
-    else:   # 比較複雜的
-        linear = [
-            2849, # 一點小激動
-            900, # 期待 愉悅 surprise
-            700, # 期待 happy
-            434, # 平靜一 nutral
-            1500, # 失望 疑惑
-            2600, # 害怕 fear
-            1100, # 快哭了 sad
-            2077, # 小聲
-        ]
-        # emo weight => happy:-1 angry:-2.2 surprise:-2 sad:4 fear:3
-        weight = [-1, -2.2, -2, 4, 3]
-        emo_rate = 4
-        # 機率＊權重
-        for idx, emotional_rate in enumerate(emo):
-                emo_rate += emotional_rate*weight[idx]
-                break        
-        if emo_rate < 0:
-            emo_rate = 0
-        elif emo_rate > len(linear)-1:
-            emo_rate = len(linear)-1
-        else:
-            emo_rate = int(round(emo_rate))
-        # 對應linear中的情緒
-        current_emotional_number = linear[emo_rate]
+    negativeLinear = [
+        2077, # 小聲
+        1500, # 失望 疑惑
+        2600, # 害怕 fear
+        1100, # 快哭了 sad
+    ]
+
+    # 分正負向
+    if(emo[1]+emo[2] == emo[3]+emo[4]):
+        # 平靜的話直接回傳434或2077
+        return 434 if round(random()) else 2077
+    else:
+        postiveFlag = 1 if (emo[1]+emo[2] > emo[3]+emo[4]) else -1
+
+    # 算權重
+    weight = [-1, 2, 3, -2, -3]
+    for idx, w in enumerate(weight):
+        if idx==0: continue # 平靜一定要是負值        
+        # 計算權重
+        weight[idx] = w*emo[idx]*postiveFlag
+
+    # 讓結果在0-3之間
+    result = round(((sum(weight)+3)/6)*3)
+    if postiveFlag==1:
+        finalEmo = postiveLinear[result]
+    else:
+        finalEmo = negativeLinear[result]
 
     # 回傳情緒代號
     emo_table = {
@@ -53,5 +48,5 @@ def get_current_emotion(emo):
         1100: '# 快哭了 sad',
         2077: '# 小聲'
     }
-    print(emo_table[current_emotional_number])
-    return current_emotional_number
+    print(emo_table[finalEmo])
+    return finalEmo
